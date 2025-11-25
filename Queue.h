a9769705233a33753b29417a80fc51b759ae3465
axumfa <umidakhmedov2603@gmail.com>
2025-11-25 14:32:58 +0500

    Queue

---
+++ Queue.h
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+#define MAX_SIZE 100
+template<typename T>
+class Queue
+{
+    private:
+        T* arr;
+        int size;
+        int front;
+        int rear;
+
+        // Default constructor
+        Queue() : arr(new T[MAX_SIZE]), size(MAX_SIZE), front(-1), rear(-1) {}
+
+        bool isEmpty() { return front == -1 || front > rear; }
+        bool isFull() { return rear == size - 1; }
+
+        T getFront()
+        {
+            if(isEmpty())
+            {
+                cout << "Queue is empty" << endl;
+                return;
+            }
+            return arr[front];
+        }
+
+        T getRear()
+        {
+            if(isEmpty())
+            {
+                cout << "Queue is empty\n";
+                return; 
+            }
+            return arr[rear];
+        }
+
+        void enqueue(T val)
+        {
+            if(isFull())
+            {
+                cout << "Queue is full\n";
+                return;
+            }
+
+            if(isEmpty())
+            {
+                front = 0;
+            }
+
+            arr[++rear] = val;
+            cout << " Element added into Queue: "<< val;
+        }
+
+        void dequeue()
+        {
+            if(isEmpty())
+            {
+                cout<<"Queue is Empty\n";
+                return;
+            }
+
+            T *ptr = arr[front--];
+            
+            // I should check whether deletion will be or not it can cause Memory Leak
+            delete ptr; 
+        }
+};
